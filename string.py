e565e7a56fd79a7852147d5e3536dcdbf0a08c36
brainseeddeveloper <brainseed.developer@gmail.com>
2019-09-14 12:14:21 +0630

    List

---
+++ string.py
@@ -0,0 +1,57 @@
+#Week 2A 14.9.2019
+
+print('"I can do everything Nothing Impossible"')
+print("'I can do everything Nothing Impossible'")
+
+x ="hello world"
+print(x)
+
+>>> 5 * 'Hello' +  'World'
+'HelloHelloHelloHelloHelloWorld'
+>>> x = 'Hello' + 'World'
+>>> x
+'HelloWorld'
+>>> 'Hello' + 1 
+>>> 5 * 'Hello'
+
+
+P r o g r a m  m i  n g
+0 1 2 3 4 5 6  7 8  9 10
+			 -4 -3 -2-1
+
+>>> x = "Programming"
+>>> x[6]
+>>> x[0:6]
+>>> x[:7]
+>>> x[8:]
+>>> x[:]
+>>> x[-4]
+>>> x[5:-4]
+>>> x[:3] + x[4:]
+>>> x[:3] + " " * 5 
+>>> 
+
+len = length 
+>>> len(x)
+>>> len(x) + 4
+15
+>>> len(x[5:9]) + 4
+
+x = "Programming" #string
+word = [ 1, 3, 5, 7, 9, 11, 13, 15] #list
+word = [ "P", "r", "o", "g", "r", "a", "m", "m", "i", "n", "g"]
+word[0] = "Q" 
+word[:] = "Good"
+word[2]  
+
+>>> word[:] + [2, 4, 6, 8, 10]
+
+a = [ 1, 3, 5, 7, 9]
+b = [ 2, 4, 6, 8, 10]
+c = [ "A", "B", "C", "D", "E"]
+x = a + b  + c 
+x
+x= [a, b, c]
+x
+x[1:3]
+x[1][1]
